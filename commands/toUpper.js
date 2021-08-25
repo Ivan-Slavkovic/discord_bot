4dee0e354cc4ce6a223fc469c3e0363dc7190624
Ivan-Slavkovic <ivanslavkovic.is@gmail.com>
2021-08-25 22:48:10 +0200

    Practice with strings

---
+++ commands/toUpper.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'toUpper',
+    aliases: ['UP', 'up', 'Up'],
+    description: 'Ping!',
+    cooldown: 5,
+    guildOnly: true,
+    args: true,
+    usage: false,
+    // eslint-disable-next-line no-unused-vars
+    execute(msg, args) {
+        let inputValue = args
+        for (let i = 0; i < inputValue.length; i++) {
+            inputValue[i] = inputValue[i][0].toUpperCase() + inputValue[i].slice(1)
+        }
+        msg.reply(inputValue.join(" "))
+        
+    },
+};
