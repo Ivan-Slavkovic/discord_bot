8fb2015c8fab4841f80552b2d64c2f6275e91612
Ivan-Slavkovic <ivanslavkovic.is@gmail.com>
2021-09-09 20:04:57 +0200

    4 Quotes

--- commands/quotes.js
+++ commands/quotes.js
@@ -1,15 +1,25 @@
-const { getQuote } = require('../classTasks/Q')
+const { getQuote, getQuoteMin, getQuoteMax, getQuoteMin_Max } = require('../classTasks/Q')
 module.exports = {
     name: 'randomquote',
     aliases: ['rano'],
-    description: 'Roll to win!',
+    description: 'Best smarter my man!',
     cooldown: 2,
     guildOnly: true,
     args: true,
     usage: false,
     // eslint-disable-next-line no-unused-vars
     async execute(msg, args) {
-        let k = await getQuote()
-        msg.reply(k)
+        let minLength = args[0], maxLength = args[1];
+
+        const msg1 = await getQuote()
+        const msg2 = await getQuoteMin(minLength)
+        const msg3 = await getQuoteMax(maxLength)
+        const msg4 = await getQuoteMin_Max(minLength, maxLength)
+
+        msg.reply(msg1)
+        msg.reply(msg2)
+        msg.reply(msg3)
+        msg.reply(msg4)
+        return
     },
 }
